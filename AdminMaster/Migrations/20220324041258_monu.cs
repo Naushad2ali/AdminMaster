14f47500ecefc8b53af5050a1e6e69e6782f1d3c
Naushad ali <99749562+Naushad1ali@users.noreply.github.com>
2022-04-06 10:23:05 +0530

    Add project files.

---
+++ AdminMaster/Migrations/20220324041258_monu.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace AdminMaster.Migrations
+{
+    public partial class monu : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Tbl_Users",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    F_Name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    L_Name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Gender = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Password = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Profile_Image = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    isActive = table.Column<bool>(type: "bit", nullable: false),
+                    isVerified = table.Column<bool>(type: "bit", nullable: false),
+                    Create_On = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    Update_On = table.Column<DateTime>(type: "datetime2", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Tbl_Users", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Verified_Account",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    OTP = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    UserId = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    SendTime = table.Column<DateTime>(type: "datetime2", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Verified_Account", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Tbl_Users");
+
+            migrationBuilder.DropTable(
+                name: "Verified_Account");
+        }
+    }
+}
